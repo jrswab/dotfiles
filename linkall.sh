209eff07ed57905261487155db81b39f5e7ff8d2
Jaron Swab <jrswab@gmail.com>
2018-09-15 22:41:30 -0400

    Sync with desktop edits

--- linkall.sh
+++ linkall.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # ln -s path/to/file path/to/link
-ln -sf $HOME/custom-setup/configs/redshift_config $HOME/.config/redshift.conf && echo "done"
+ln -sf $HOME/custom-setup/redshift_config $HOME/.config/redshift.conf && echo "redshift done"
+ln -sf $HOME/custom-setup/i3wm.conf $HOME/.i3/config && echo "i3wm done"
+ln -sf $HOME/custom-setup/vimrc.main $HOME/.config/nvim/init.vim && echo "nvim done"
+ln -sf $HOME/custom-setup/zshrc.main $HOME/.zshrc && echo "zsh done"
+ln -sf $HOME/custom-setup/xresources.main $HOME/.Xresources && echo "Xresources done"
