58e3c1ff08b7dccc686c7c37e5530051e548d1d8
Jaron Swab <jrswab@gmail.com>
2018-10-10 20:18:30 -0400

    updated for single temp weather

--- i3status.sh
+++ i3status.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+count=0
+
+getTemp=$(curl -s 'https://wttr.in/~15101?u0T' | tac | tac | 
+	head -n 4 | tail -n 1 | awk '{ print $2 $3 $4 }')
 
 while :
 do
-	(curl -s 'https://wttr.in/~15101?u0T' | tac | tac | head -n 4 | tail -n 1 | awk '{ print $3 $4 }';echo ' | ';date +%b' '%d' '%H':'%M)|tr -d '\n'
-	sleep 30
+	# send data to output
+	(echo "${getTemp}";echo ' | ';date +%b' '%d' '%H':'%M)|tr -d '\n'
+	
+	# get weather update every hour
+	count=$((count + 1))
+	if ! ((count % 3600)); then
+		getTemp=$(curl -s 'https://wttr.in/~15101?u0T' | tac | tac | 
+			head -n 4 | tail -n 1 | awk '{ print $3 $4 }')
+	fi
+
+	sleep 1
 done
