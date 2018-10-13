34a12dd9a001cfed2f2210bde45864c003fc1554
Jaron Swab <jrswab@gmail.com>
2018-10-13 07:40:11 -0400

    updated temp command and cleaned up directory.

---
+++ i3/status.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+count=0
+
+getTemp=$(curl -s 'https://wttr.in/~15101?u0T' | tac | tac | head -n 4 | tail -n 1 | sed 's/ //g' | sed -r 's/[-0-9]+°F/&~&/g' | cut -d '~' -f2)
+
+while :
+do
+	# send data to output
+	(echo "${getTemp}";echo ' | ';date +%b' '%d' '%H':'%M)|tr -d '\n'
+	
+	# get weather update every hour
+	count=$((count + 1))
+	if ! ((count % 3600)); then
+		getTemp=$(curl -s 'https://wttr.in/~15101?u0T' | tac | tac | head -n 4 | tail -n 1 | sed 's/ //g' | sed -r 's/[-0-9]+°F/&~&/g' | cut -d '~' -f2)
+	fi
+
+	sleep 1
+done
--- linkall.sh
+++ linkall.sh
@@ -2,8 +2,8 @@
 
 # ln -s path/to/file path/to/link
 ln -sf $HOME/custom-setup/redshift_config $HOME/.config/redshift.conf && echo "redshift done"
-ln -sf $HOME/custom-setup/i3wm.conf $HOME/.i3/config && echo "i3wm done"
+ln -sf $HOME/custom-setup/i3/i3wm.conf $HOME/.i3/config && echo "i3wm done"
+ln -sf $HOME/custom-setup/i3/blocks.conf $HOME/.i3/i3blocks.conf && echo "i3Blocks done"
 ln -sf $HOME/custom-setup/vimrc.main $HOME/.config/nvim/init.vim && echo "nvim done"
 ln -sf $HOME/custom-setup/zshrc.main $HOME/.zshrc && echo "zsh done"
 ln -sf $HOME/custom-setup/xresources.main $HOME/.Xresources && echo "Xresources done"
-ln -sf $HOME/custom-setup/i3blocks.conf $HOME/.i3/i3blocks.conf && echo "i3Blocks done"
