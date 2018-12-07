ce98f54ad9be698645659e342dde0896570870a4
Jaron Swab <jrswab@gmail.com>
2018-12-06 20:12:48 -0500

    added highest mem prog, ever 30 seconds

--- i3/status.sh
+++ i3/status.sh
@@ -3,17 +3,24 @@ count=0
 
 getTemp=$(curl -s 'https://wttr.in/~15101?u0T' | tac | tac | head -n 4 | tail -n 1 | sed 's/ //g' | sed -r 's/[-0-9]+°F/&~&/g' | cut -d '~' -f2)
 
+ram=$(free -h | awk '/^Mem:/ {print $3 "/" $2}')
+maxRam=$(ps axch -o cmd --sort=-%mem | sed 1q)
+
 while :
 do
-	# get RAM
-	ram=$(free -h | awk '/^Mem:/ {print $3 "/" $2}')
 	# send data to output
-	(echo "${ram}";echo ' | ';echo "${getTemp}";echo ' | ';date +%b' '%d' '%H':'%M)|tr -d '\n'
+	(echo "${ram}";echo " ${maxRam}";echo ' | ';echo "${getTemp}";echo ' | ';date +%b' '%d' '%H':'%M)|tr -d '\n'
 	
 	# get weather update every hour
 	count=$((count + 1))
 	if ! ((count % 3600)); then
 		getTemp=$(curl -s 'https://wttr.in/~15101?u0T' | tac | tac | head -n 4 | tail -n 1 | sed 's/ //g' | sed -r 's/[-0-9]+°F/&~&/g' | cut -d '~' -f2)
+	fi
+
+	if ! ((count % 30)); then
+		# get RAM
+		ram=$(free -h | awk '/^Mem:/ {print $3 "/" $2}')
+		maxRam=$(ps axch -o cmd --sort=-%mem | sed 1q)
 	fi
 
 	sleep 1
