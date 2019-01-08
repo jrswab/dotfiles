5d9075ec575b70dfe4a91fd7931a0cce7e6e0950
Jaron Swab <jrswab@gmail.com>
2019-01-07 21:37:06 -0500

    fixed status script and reset i3 scratchpad to minus key

--- i3/status.sh
+++ i3/status.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Set Initial Conditions
 count=0
 
 getTemp=$(curl -s 'https://wttr.in/~15101?u0T' | tac | tac | head -n 4 | tail -n 1 | sed 's/ //g' | sed -r 's/[-0-9]+°F/&~&/g' | cut -d '~' -f2)
@@ -10,11 +11,7 @@ appRam=$(ps axch -o %mem --sort=-%mem | sed 1q)
 
 while :
 do
-	# send data to output
-	(echo " ${maxRam}";echo "$appRam% of ${ram}";echo ' | ';echo "${getTemp}";echo ' | ';date +%b' '%d' '%H':'%M)|tr -d '\n'
-	
 	# get weather update every hour
-	count=$((count + 1))
 	if ! ((count % 3600)); then
 		getTemp=$(curl -s 'https://wttr.in/~15101?u0T' | tac | tac | head -n 4 | tail -n 1 | sed 's/ //g' | sed -r 's/[-0-9]+°F/&~&/g' | cut -d '~' -f2)
 	fi
@@ -26,5 +23,9 @@ do
 		appRam=$(ps axch -o %mem --sort=-%mem | sed 1q)
 	fi
 
+	# send data to output
+	(echo " ${maxRam}";echo "$appRam% of ${ram}";echo ' | ';echo "${getTemp}";echo ' | ';date +%b' '%d' '%H':'%M)|tr -d '\n'
+
+	count=$((count + 1))
 	sleep 1
 done
