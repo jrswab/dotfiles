5a61dee8aac4e400402be0c320fefae8fa1ef04f
J. R. Swab <jrswab@gmail.com>
2019-07-02 21:09:39 -0400

    Added "echo"...

--- sudoLink.sh
+++ sudoLink.sh
@@ -2,4 +2,4 @@
 dir="$(pwd)";
 # ln -s path/to/file path/to/link
 ln -sf "$dir"/surf/surfLauncher.sh /usr/local/bin/s && echo "surf done"
-ln -sf "$dir"/dwm/dwm.desktop /usr/share/xsessions/dwm.desktop && "DWM login option created"
+ln -sf "$dir"/dwm/dwm.desktop /usr/share/xsessions/dwm.desktop && echo "DWM login option created"
