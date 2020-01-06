7da95254d5a142e592c57914ade9b0267a603911
Jaron Swab <jrswab@gmail.com>
2020-01-06 15:42:37 -0500

    Massive reorganization of files.
    Moved files into directories and removed files no longer in use.
    
    Also added Polybar files and update linkall.sh to include BSPWM/SXHKD
    configs.

--- sudoLink.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-dir="$(pwd)";
-# ln -s path/to/file path/to/link
-ln -sf "$dir"/surfLauncher.sh /usr/local/bin/s && echo "surf done"
--- surfLauncher.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-surf -pgm "$1" # disable plugins and geolocation; cookie policy set in config.h
