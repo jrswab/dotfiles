8e09b391a124c0546c05cc2afb04a477dac5294b
Jaron Swab <jrswab@gmail.com>
2020-01-26 12:58:54 -0500

    Added check for Rio window manager to zshrc.
    This way it will auto switch to the RC shell when runnig Rio.
    As Plan9 intended.

--- polybar/launchPoly.sh
+++ polybar/launchPoly.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Terminate already running Breto instance (github.com/jrswab/breto)
+killall -q breto
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null ; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar main
