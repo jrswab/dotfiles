03434e610b65f6524b3177a596bb1a4e5f47f493
Jaron Swab <jrswab@gmail.com>
2020-03-10 19:19:25 -0400

    Added emacs init.el
    updated linkall.sh to include emacs init.el soflink

---
+++ emacs/init.el
@@ -0,0 +1,53 @@
+(require 'package)
+ 
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+ 
+(setq package-enable-at-startup nil)
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (go-errcheck which-key go-mode nord-theme evil-visual-mark-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Vim keybindings
+(require 'evil)
+(evil-mode t)
+
+;; Set GUI window size:
+(add-to-list 'default-frame-alist '(height . 50))
+(add-to-list 'default-frame-alist '(width . 100))
+(set-default-font "Hack 12")
+
+;; Nord Color Theme
+(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
+(load-theme 'nord t)
+
+;; Packages
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+;; Package List:
+(use-package markdown-mode
+  :ensure t)
+(use-package go-mode
+  :ensure t)
+(use-package which-key
+  :ensure t)
+(use-package go-errcheck
+  :ensure t)
