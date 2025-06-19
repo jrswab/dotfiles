f71e4d82a647e72658e9ed516bee77956beb2331
Jaron Swab <jr@jrswab.com>
2025-06-19 13:18:17 -0400

    Tone down color scheme highlighting

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -4,4 +4,21 @@ return {
 	version = false,
 	lazy = false,
 	priority = 1000, -- load this before all the other start plugins
+
+	config = function()
+		require("everforest").setup({
+			-- “Calm the rainbow”
+			colours_override = function(palette)
+				local calm     = "#D3C6AA"
+
+				palette.aqua   = calm
+				palette.blue   = calm
+				palette.purple = calm
+				-- (red, yellow, orange and green stay as-is)
+			end,
+		})
+
+		-- load the colorscheme
+		require("everforest").load()
+	end,
 }
